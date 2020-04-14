9dcd32fd2ed4ff9c21fe098feaca999b68de8e10
Pascal Audet <pascal.audet@uottawa.ca>
2020-04-14 11:52:58 -0400

    added initial tests and hopefully fixed travis deploy to gh-pages

---
+++ orientpy/tests/test_0_imports.py
@@ -0,0 +1,18 @@
+def test_stdb_import():
+    import stdb
+
+def test_obspy_import():
+    import obspy
+
+def test_orientpy_modules():
+    import orientpy
+    from orientpy import Orient
+    from orientpy import DL
+    from orientpy import BNG
+    from orientpy import classes
+    from orientpy import utils
+    from orientpy import plotting
+    from orientpy import io
+    from orientpy import arguments
+    import matplotlib
+    matplotlib.use('Agg')
