8aa46fb08ee7121a66240973ee50911f7a5f10ea
Pascal Audet <pascal.audet@uottawa.ca>
2020-05-09 17:19:22 -0400

    fixed test for gvmap

--- orientpy/tests/test_DL.py
+++ orientpy/tests/test_DL.py
@@ -2,7 +2,6 @@ import stdb
 import numpy as np
 from pkg_resources import resource_filename
 from orientpy import DL, arguments, utils
-from pathlib import Path
 from obspy.clients.fdsn import Client
 
 
@@ -30,14 +29,20 @@ def get_cat():
 
 def test_gvmap():
 
-    gvpath = Path('../dispmaps')
-    map10 = np.loadtxt(gvpath / 'R.gv.10.txt')
-    map15 = np.loadtxt(gvpath / 'R.gv.15.txt')
-    map20 = np.loadtxt(gvpath / 'R.gv.20.txt')
-    map25 = np.loadtxt(gvpath / 'R.gv.25.txt')
-    map30 = np.loadtxt(gvpath / 'R.gv.30.txt')
-    map35 = np.loadtxt(gvpath / 'R.gv.35.txt')
-    map40 = np.loadtxt(gvpath / 'R.gv.40.txt')
+    map10 = np.loadtxt(resource_filename('orientpy',
+                            'dispmaps/R.gv.10.txt'))
+    map15 = np.loadtxt(resource_filename('orientpy',
+                            'dispmaps/R.gv.15.txt'))
+    map20 = np.loadtxt(resource_filename('orientpy',
+                            'dispmaps/R.gv.20.txt'))
+    map25 = np.loadtxt(resource_filename('orientpy',
+                            'dispmaps/R.gv.25.txt'))
+    map30 = np.loadtxt(resource_filename('orientpy',
+                            'dispmaps/R.gv.30.txt'))
+    map35 = np.loadtxt(resource_filename('orientpy',
+                            'dispmaps/R.gv.35.txt'))
+    map40 = np.loadtxt(resource_filename('orientpy',
+                            'dispmaps/R.gv.40.txt'))
     assert map10 is not None, 'Failed! error in loading map10 disp map'
     assert map15 is not None, 'Failed! error in loading map15 disp map'
     assert map20 is not None, 'Failed! error in loading map20 disp map'
