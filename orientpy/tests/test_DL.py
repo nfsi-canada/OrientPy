dd1b5e37e7cc62bb39ff6cb763a518972de90446
Pascal Audet <pascal.audet@uottawa.ca>
2020-05-10 13:08:41 -0400

    changed paths to Path objects

--- orientpy/tests/test_DL.py
+++ orientpy/tests/test_DL.py
@@ -7,9 +7,10 @@ from obspy.clients.fdsn import Client
 
 def get_stdb():
     dbfile = resource_filename('orientpy',
-                                'examples/data/LOBS3.pkl')
-    db = stdb.io.load_db(dbfile)   
+                               'examples/data/LOBS3.pkl')
+    db = stdb.io.load_db(dbfile)
     return db['YH.LOBS3']
+
 
 def get_cat():
 
@@ -27,22 +28,23 @@ def get_cat():
 
     return cat
 
+
 def test_gvmap():
 
     map10 = np.loadtxt(resource_filename('orientpy',
-                            'dispmaps/R.gv.10.txt'))
+                                         'dispmaps/R.gv.10.txt'))
     map15 = np.loadtxt(resource_filename('orientpy',
-                            'dispmaps/R.gv.15.txt'))
+                                         'dispmaps/R.gv.15.txt'))
     map20 = np.loadtxt(resource_filename('orientpy',
-                            'dispmaps/R.gv.20.txt'))
+                                         'dispmaps/R.gv.20.txt'))
     map25 = np.loadtxt(resource_filename('orientpy',
-                            'dispmaps/R.gv.25.txt'))
+                                         'dispmaps/R.gv.25.txt'))
     map30 = np.loadtxt(resource_filename('orientpy',
-                            'dispmaps/R.gv.30.txt'))
+                                         'dispmaps/R.gv.30.txt'))
     map35 = np.loadtxt(resource_filename('orientpy',
-                            'dispmaps/R.gv.35.txt'))
+                                         'dispmaps/R.gv.35.txt'))
     map40 = np.loadtxt(resource_filename('orientpy',
-                            'dispmaps/R.gv.40.txt'))
+                                         'dispmaps/R.gv.40.txt'))
     assert map10 is not None, 'Failed! error in loading map10 disp map'
     assert map15 is not None, 'Failed! error in loading map15 disp map'
     assert map20 is not None, 'Failed! error in loading map20 disp map'
@@ -51,6 +53,7 @@ def test_gvmap():
     assert map35 is not None, 'Failed! error in loading map35 disp map'
     assert map40 is not None, 'Failed! error in loading map40 disp map'
 
+
 def test_init_DL():
 
     sta = get_stdb()
@@ -58,18 +61,18 @@ def test_init_DL():
     assert isinstance(dl, DL), 'Failed initializing DL object'
     return dl
 
+
 def test_add_cat():
 
     dl = test_init_DL()
     cat = get_cat()
     for ev in [cat[0]]:
-        # Add event to object
         accept = dl.add_event(
             ev, gacmin=0., gacmax=180.,
             depmax=40., returned=True)
         assert accept, 'event not accepted'
-
     return dl
+
 
 def test_add_data():
 
@@ -78,10 +81,11 @@ def test_add_data():
     t2 = 4.*60.*60.
     has_data = dl.download_data(
         client=Client(), stdata=[],
-        ndval=0., new_sr=2., t1=t1, t2=t2, 
+        ndval=0., new_sr=2., t1=t1, t2=t2,
         returned=True, verbose=False)
     assert has_data, 'no data'
     return dl
+
 
 def test_calc():
 
@@ -91,4 +95,3 @@ def test_calc():
     assert dl.meta.R2phi is not None, 'R2phi is None'
     assert dl.meta.R1cc is not None, 'R1cc is None'
     assert dl.meta.R2cc is not None, 'R2cc is None'
-
