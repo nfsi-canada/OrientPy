cec31240013fee6bdaf8b8042f2abfe719ce2c1f
Pascal Audet <pascal.audet@uottawa.ca>
2020-05-12 09:25:16 -0400

    fixed dl plotting function for testing

--- orientpy/plotting.py
+++ orientpy/plotting.py
@@ -43,6 +43,7 @@ def plot_bng_waveforms(bng, stream, dts, tt):
             bng.meta.TR, bng.meta.RZ)
     plt.suptitle(text, fontsize=10)
     plt.subplots_adjust(hspace=0.05)
+
     return plt
 
 
@@ -222,8 +223,5 @@ def plot_dl_results(stkey, R1phi, R1cc, R2phi, R2cc, ind, val,
     plt.suptitle(text, fontsize=12)
     gs.tight_layout(f, rect=[0, 0, 1, 0.95])
 
-    # save figure
-    if loc:
-        plt.savefig(loc+'results.'+fmt, fmt=fmt)
-
-    plt.show()
+    return plt
+    
