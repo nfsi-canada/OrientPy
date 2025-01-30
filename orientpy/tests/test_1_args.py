855676591f9c1d5b72d2a309922526a98fb55db0
Pascal Audet <pascal.audet@uottawa.ca>
2025-01-30 18:50:58 +1300

    fixing deploy workflow 6

--- orientpy/tests/test_1_args.py
+++ orientpy/tests/test_1_args.py
@@ -7,13 +7,12 @@ import pytest
 dbfile = resource_filename('orientpy',
                            'examples/data/LOBS3.pkl')
 
+
 def test_dl_calc_args():
     from orientpy.scripts import dl_calc as dl
     # no stdb
-    with pytest.raises(SystemExit) as excinfo:
-        dl.get_dl_calc_arguments()
-        assert excinfo.value.code == 1
-
+    with pytest.raises(SystemExit):
+        dl.get_dl_calc_arguments([])
     # defaults
     args0 = dl.get_dl_calc_arguments([dbfile])
     # keys
@@ -22,24 +21,21 @@ def test_dl_calc_args():
     # start time
     args = dl.get_dl_calc_arguments(
         [dbfile, '--start', '2014-10-01'])
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         dl.get_dl_calc_arguments([
             dbfile, '--start', 'abcd'])
-        assert excinfo.value.code == 1
     # end time
     args = dl.get_dl_calc_arguments([
         dbfile, '--end', '2015-01-01'])
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         dl.get_dl_calc_arguments([
             dbfile, '--end', 'abcd'])
-        assert excinfo.value.code == 1
     # user auth.
     args = dl.get_dl_calc_arguments([
         dbfile, '-U', 'bla:bla'])
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         dl.get_dl_calc_arguments([
             dbfile, '-U', 'abcd'])
-        assert excinfo.value.code == 1
     # local data
     args = dl.get_dl_calc_arguments([
         dbfile, '--local-data', 'bla'])
@@ -49,12 +45,12 @@ def test_dl_calc_args():
 
     return args0
 
+
 def test_dl_average_args():
     from orientpy.scripts import dl_average as dl
     # no stdb
-    with pytest.raises(SystemExit) as excinfo:
-        dl.get_dl_average_arguments()
-        assert excinfo.value.code == 1
+    with pytest.raises(SystemExit):
+        dl.get_dl_average_arguments([])
     # defaults
     args0 = dl.get_dl_average_arguments([dbfile])
     # keys
@@ -66,9 +62,8 @@ def test_dl_average_args():
 def test_bng_calc_args():
     from orientpy.scripts import bng_calc_auto as bng
     # no stdb
-    with pytest.raises(SystemExit) as excinfo:
-        bng.get_bng_calc_arguments()
-        assert excinfo.value.code == 1
+    with pytest.raises(SystemExit):
+        bng.get_bng_calc_arguments([])
     # default
     args0 = bng.get_bng_calc_arguments([dbfile])
     # keys
@@ -77,24 +72,21 @@ def test_bng_calc_args():
     # start time
     args = bng.get_bng_calc_arguments(
         [dbfile, '--start', '2014-10-01'])
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         bng.get_bng_calc_arguments([
             dbfile, '--start', 'abcd'])
-        assert excinfo.value.code == 1
     # end time
     args = bng.get_bng_calc_arguments([
         dbfile, '--end', '2015-01-01'])
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         bng.get_bng_calc_arguments([
             dbfile, '--end', 'abcd'])
-        assert excinfo.value.code == 1
     # user auth.
     args = bng.get_bng_calc_arguments([
         dbfile, '-U', 'bla:bla'])
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         bng.get_bng_calc_arguments([
             dbfile, '-U', 'abcd'])
-        assert excinfo.value.code == 1
     # local data
     args = bng.get_bng_calc_arguments([
         dbfile, '--local-data', 'bla'])
@@ -104,26 +96,24 @@ def test_bng_calc_args():
     # bp
     args = bng.get_bng_calc_arguments([
         dbfile, '--bp', '1.,2.'])
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         bng.get_bng_calc_arguments([
             dbfile, '--bp', '1.'])
-        assert excinfo.value.code == 1
     # tt
     args = bng.get_bng_calc_arguments([
         dbfile, '--times', '1.,2.'])
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         bng.get_bng_calc_arguments([
             dbfile, '--times', '1.'])
-        assert excinfo.value.code == 1
 
     return args0
+
 
 def test_bng_average_args():
     from orientpy.scripts import bng_average as bng
     # no stdb
-    with pytest.raises(SystemExit) as excinfo:
-        bng.get_bng_average_arguments()
-        assert excinfo.value.code == 1
+    with pytest.raises(SystemExit):
+        bng.get_bng_average_arguments([])
     # defaults
     args0 = bng.get_bng_average_arguments([dbfile])
     # keys
